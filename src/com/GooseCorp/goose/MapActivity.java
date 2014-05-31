6772eb5d2f94566b9527b58b5cdea96505099c49
Chinmay <chinmayhamin@gmail.com>
2014-05-31 12:33:20 -0400

    marker

--- src/com/GooseCorp/goose/MapActivity.java
+++ src/com/GooseCorp/goose/MapActivity.java
@@ -1,15 +1,33 @@
 package com.GooseCorp.goose;
 
 import android.app.Activity;
-
-import android.app.Activity;
 import android.os.Bundle;
 
-public class MapActivity extends Activity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class MapActivity extends Activity {
+	
+	GoogleMap map;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(41.889, -87.622), 16));
+
+        // You can customize the marker image using images bundled with
+        // your app, or dynamically generated bitmaps. 
+        map.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.busstop))
+                .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+                .position(new LatLng(41.889, -87.622)));
     }
 }
