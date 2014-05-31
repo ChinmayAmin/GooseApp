6113265bbdfb292ba6159ae9e885bd59603c169d
Chinmay <chinmayhamin@gmail.com>
2014-05-31 13:11:42 -0400

    Added map marker

--- src/com/GooseCorp/goose/MapActivity.java
+++ src/com/GooseCorp/goose/MapActivity.java
@@ -1,33 +1,65 @@
 package com.GooseCorp.goose;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends Activity {
+public class MapActivity extends Activity implements LocationListener {
 	
 	GoogleMap map;
+	private LocationManager locationManager;
+	private String provider;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        
+        //Get the map
         map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                new LatLng(41.889, -87.622), 16));
-
-        // You can customize the marker image using images bundled with
-        // your app, or dynamically generated bitmaps. 
-        map.addMarker(new MarkerOptions()
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.busstop))
-                .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
-                .position(new LatLng(41.889, -87.622)));
+        //Check if the user is using wifi gps enabled
+        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+        
+        //Check if gps is enabled
+        boolean enabledGPS = service
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+        
+        //Check if wifi enabled
+        boolean enabledWiFi = service
+                .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        
+        //If gps is not on then error message
+        if (!enabledGPS) {
+            Toast.makeText(this, "GPS signal not found", Toast.LENGTH_LONG).show();
+        }
+        else if(!enabledWiFi){
+        	Toast.makeText(this, "GPS signal not found", Toast.LENGTH_LONG).show();
+        }
+       
     }
+
+	@Override
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+		Location loc = arg0;
+        map.addMarker(new MarkerOptions()
+        .icon(BitmapDescriptorFactory.fromResource(R.drawable.busstop))
+        .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+        .position(new LatLng(45.4000,75.6667)));
+	}
 }
