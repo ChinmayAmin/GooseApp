f4a93d55ca64339e5e2fe906b8d425194ba8f252
Chinmay <chinmayhamin@gmail.com>
2014-05-31 12:04:46 -0400

    Back to colorfull front page

--- src/com/GooseCorp/goose/MainActivity.java
+++ src/com/GooseCorp/goose/MainActivity.java
@@ -1,15 +1,15 @@
 package com.GooseCorp.goose;
 
-import android.app.Activity;
 import android.app.ActionBar;
-import android.app.Fragment;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -17,11 +17,67 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		
+		//Remove the action bar
+				ActionBar actionBar = getActionBar();
+				actionBar.setTitle("Goose");
+				actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#6db5ff")));
+				actionBar.show();
+				
+				//Get the spinners and the buttons
+				Button mapButton = (Button)findViewById(R.id.goToMap);
+				Button searchButton = (Button)findViewById(R.id.search);
+				Button savedButton = (Button)findViewById(R.id.savedStops);
+				Button aboutButton = (Button)findViewById(R.id.about);
+				
+				//Set the background
+				mapButton.setBackground(this.getResources().getDrawable(R.drawable.mapicon));
+				searchButton.setBackground(this.getResources().getDrawable(R.drawable.search));
+				savedButton.setBackground(this.getResources().getDrawable(R.drawable.bookmarks));
+				aboutButton.setBackground(this.getResources().getDrawable(R.drawable.aboutus));
+				
+				//User clicked on search(find by stop number of bus)
+				searchButton.setOnClickListener(new View.OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+//						Intent intent = new Intent(MainActivity.this,SearchActivity.class);
+//						startActivity(intent);
+					}
+				});
+				
+				//Bookmarks
+				savedButton.setOnClickListener(new View.OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+//						Intent intent = new Intent(MainActivity.this,BookmarkActivity.class);
+//						startActivity(intent);
+					}
+				});
+				
+				//About us
+				aboutButton.setOnClickListener(new View.OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+//						Intent intent = new Intent(MainActivity.this,AboutActivity.class);
+//						startActivity(intent);
+					}
+				});
+				//User clicked on map. Go to Map Activity
+				mapButton.setOnClickListener(new View.OnClickListener() {
+					
+					@Override
+					public void onClick(View arg0) {
+						// TODO Auto-generated method stub
+//						Intent intent = new Intent(MainActivity.this,SearchActivity.class);
+//						startActivity(intent);
+					}
+				});
 	}
 
 	@Override
@@ -43,22 +99,4 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-	}
-
 }
